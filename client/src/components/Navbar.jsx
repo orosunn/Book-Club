2b3887dd214a0fbc33a711ea18223dfe73e95150
paulina koch <pjassakoch@gmail.com>
2024-02-09 15:50:19 -0500

    Fixed navbar error message appearing in dev tools , chaged 'activeClassName' to className

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import { NavLink, useLocation } from 'react-router-dom';
 
 function NavBar() {
+  const getClassName = ({ isActive }) => isActive ? "item active" : "item";
   return (
     <div className="ui brown three item menu">
-      <NavLink exact to="/" activeClassName="active" className="item">
+      <NavLink exact to="/" className={getClassName} end>
         Home
       </NavLink>
-      <NavLink to="/books" activeClassName="active" className="item">
+      <NavLink to="/books" className={getClassName}end>
         Books
       </NavLink>
-      <NavLink to="/discussion" activeClassName="active" className="item">
+      <NavLink to="/discussion" className={getClassName}>
         Discussion
       </NavLink>
     </div>
